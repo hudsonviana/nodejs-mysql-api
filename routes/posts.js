d768877f7ef7825984976ea98497c55520e53bab
hudsonviana <hudson1206@gmail.com>
2023-11-03 19:59:24 -0300

    Implement CRUD - Create, Read, Update and Delete

--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const postController = require('../controllers/post.controller');
 
 router.post('/', postController.save);
+router.get('/:id', postController.show);
+router.get('/', postController.index);
+router.patch('/:id', postController.update);
+router.delete('/:id', postController.destroy);
 
 module.exports = router;
